5dd00772022c3d9fb10073e51f19b1f7e92e550d
Patrick Carver <patrickcarver@gmail.com>
2025-12-06 03:54:32 -0600

    finish

--- lib/day06.ex
+++ lib/day06.ex
@@ -1,18 +1,80 @@
 defmodule Day06 do
-  @moduledoc """
-  Documentation for `Day06`.
-  """
+  def part1(file_name) do
+    file_name
+    |> simple_parse()
+    |> pivot()
+    |> Enum.reduce(0, fn list, total ->
+      [op | nums] = list |> Enum.reverse()
 
-  @doc """
-  Hello world.
+      {result, []} =
+        nums |> Enum.intersperse(op) |> Enum.join() |> Code.eval_string()
 
-  ## Examples
+      total + result
+    end)
+  end
 
-      iex> Day06.hello()
-      :world
+  def part2(file_name) do
+    lines = lines(file_name)
+    [ops | nums] = Enum.reverse(lines)
 
-  """
-  def hello do
-    :world
+    ops = String.split(ops, " ", trim: true)
+    nums = Enum.reverse(nums)
+
+    longest_num =
+      nums |> Enum.map(&longest_number/1) |> Enum.max()
+
+    nums
+    |> Enum.map(fn line ->
+      line
+      |> String.graphemes()
+      |> then(fn graphemes ->
+        list_size = length(graphemes)
+        needed_padding = rem(list_size, longest_num)
+        needed_padding = if needed_padding == 0, do: 0, else: longest_num - needed_padding
+        padding = List.duplicate(" ", needed_padding)
+        graphemes ++ padding
+      end)
+    end)
+    |> pivot()
+    |> Enum.map(fn list -> list |> Enum.join() |> String.trim() end)
+    |> Enum.chunk_by(fn item -> item == "" end)
+    |> Enum.reject(fn item -> item == [""] end)
+    |> Enum.zip(ops)
+    |> Enum.reduce(0, fn {nums, op}, total ->
+      {result, []} =
+        nums |> Enum.intersperse(op) |> Enum.join() |> Code.eval_string()
+
+      total + result
+    end)
+  end
+
+  def longest_number(line) do
+    Regex.scan(~r/\d+/, line, capture: :all)
+    |> List.flatten()
+    |> Enum.sort_by(&String.length/1, :desc)
+    |> hd()
+    |> String.length()
+  end
+
+  def lines(file_name) do
+    file_name
+    |> File.stream!()
+    |> Stream.map(&String.trim_trailing/1)
+  end
+
+  def simple_parse(file_name) do
+    file_name
+    |> File.stream!()
+    |> Stream.map(fn line ->
+      line
+      |> String.trim_trailing()
+      |> String.split(" ", trim: true)
+    end)
+  end
+
+  def pivot(list) do
+    list
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
   end
 end
