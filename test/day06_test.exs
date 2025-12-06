33895cd4c4b491a3b01eb81b2e5ec09ee8e547d1
Patrick Carver <patrickcarver@gmail.com>
2025-12-06 12:45:33 -0600

    add test for part 1 and 2

--- test/day06_test.exs
+++ test/day06_test.exs
@@ -1,8 +1,13 @@
 defmodule Day06Test do
   use ExUnit.Case
-  doctest Day06
 
-  test "greets the world" do
-    assert Day06.hello() == :world
+  @test_file "priv/test.txt"
+  @input_file "priv/input.txt"
+
+  test "it actually works" do
+    assert Day06.part1(@test_file) == 4_277_556
+    assert Day06.part1(@input_file) == 4_719_804_927_602
+    assert Day06.part2(@test_file) == 3_263_827
+    assert Day06.part2(@input_file) == 9_608_327_000_261
   end
 end
